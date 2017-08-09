62f2ef373793efafad1d236de0c11465c930dcd4
Richard Gowers <richardjgowers@gmail.com>
2017-08-09 19:38:16 +0100

    Added stride function and test

--- pystride/stride.py
+++ pystride/stride.py
@@ -1,12 +1,20 @@
-def stride():
+import subprocess
+
+
+def stride(pdbpath):
     """Perform STRIDE analysis
 
     Parameters
     ----------
-
+    pdbpath : str
+      path to the pdb file to analyse
 
     Returns
     -------
     """
-    pass
+    p = subprocess.Popen(['stride', pdbpath],
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
 
+    return stdout
--- tests/test_stride.py
+++ tests/test_stride.py
@@ -1,1 +1,27 @@
 """Tests for python bindings to STRIDE"""
+
+import os
+import pytest
+import pystride
+from six.moves import zip
+
+HERE = os.path.dirname(os.path.abspath(__file__))
+
+@pytest.fixture
+def ADK():
+    return os.path.join(HERE, 'adk_oplsaa.pdb')
+
+
+@pytest.fixture
+def REF_ADK():
+    with open(os.path.join(HERE, 'adk.out'), 'r') as inf:
+        data = inf.read()
+    return data
+
+
+def test_stride(ADK, REF_ADK):
+    result = pystride.stride(ADK)
+    for a, b in zip(result.split('\n'), REF_ADK.split('\n')):
+        if a.startswith('CHN'):
+            continue
+        assert a == b
